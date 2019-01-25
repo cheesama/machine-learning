bd4b9aaee260bd88cad9fba68f29369e7a8dcc5d
cheesama <cheehoon12@hanmail.net>
2019-01-25 15:34:01 +0900

    implement bayesian optimization way

--- NLP/sentiment_analysis/dynamic_embedding_sentiment_classification/sentiment_data_loader.py
+++ NLP/sentiment_analysis/dynamic_embedding_sentiment_classification/sentiment_data_loader.py
@@ -9,20 +9,22 @@ def postprocess(x, train=True):
     x = int(x)
     return x
 
-def create_data_loader(filePath, batchSize=128, tokenizer=twitter_tokenizer, build_vocab=False, device=-1):
+def create_data_loader(train_file_path, val_file_path, batchSize=128, tokenizer=twitter_tokenizer, device=-1):
     text_field = data.Field(tokenize=tokenizer, sequential=True, init_token='[CLS]', eos_token='[SEP]')
     label_field = data.Field(sequential=False, use_vocab=False, postprocessing=data.Pipeline(postprocess))
 
-    dataset = data.TabularDataset(path=filePath, format='tsv', fields=[('id', None), ('document', text_field), ('label', label_field)], filter_pred=lambda ex: ex.label in ['0', '1'])
-    dataLoader = data.Iterator(dataset=dataset, batch_size=batchSize, sort_key=lambda x: len(x.document), train=True, repeat=False, device=device)
+    train_dataset = data.TabularDataset(train_file_path, format='tsv', fields=[('id', None), ('document', text_field), ('label', label_field)], filter_pred=lambda ex: ex.label in ['0', '1'])
+        
+    print('Building Vocabulary of file - ' + train_file_path)
+    text_field.build_vocab(train_dataset)
+
+    vocab_size = len(text_field.vocab)
+
+    train_dataLoader = data.Iterator(dataset=train_dataset, batch_size=batchSize, sort_key=lambda x: len(x.document), train=True, repeat=False, device=device)
+
+    val_dataset = data.TabularDataset(val_file_path, format='tsv', fields=[('id', None), ('document', text_field), ('label', label_field)], filter_pred=lambda ex: ex.label in ['0', '1'])
+    val_dataLoader = data.Iterator(dataset=val_dataset, batch_size=batchSize, sort=False, train=False, device=device)
     
-    if build_vocab:
-        print('Building Vocabulary of file - ' + filePath)
-        text_field.build_vocab(dataset)
-        vocab_size = len(text_field.vocab)
-
-        return dataLoader, vocab_size
-
-    return dataLoader
+    return train_dataLoader, val_dataLoader, vocab_size
 
 
--- NLP/sentiment_analysis/dynamic_embedding_sentiment_classification/train_eval.py
+++ NLP/sentiment_analysis/dynamic_embedding_sentiment_classification/train_eval.py
@@ -5,45 +5,67 @@ from argparse import ArgumentParser
 from tqdm import tqdm
 from torch.nn import DataParallel
 from torch.optim import SGD
+from bayes_opt import BayesianOptimization
 
 import torch
 import torch.nn as nn
 import mlflow
 
 class Sentiment_train_eval(object):
-    def __init__(self, train_loader, val_loader, optimizer, loss_fn, config):
+    def __init__(self, train_loader, val_loader, model, loss_fn, config, bayes_opt=False):
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.model = model
-        self.optimizer = optimizer
         self.loss_fn = loss_fn
         self.config = config
 
+    def bayes_opt_train_eval(self):
+        pbounds = {}
+        for attr, value in self.config.items():
+            if isinstance(value, tuple):
+                pbounds[attr] = value
+                #self.config.pop(attr, None)
+
+        bayes_optimizer = BayesianOptimization(f=self.bayes_opt_config_wrapper, pbounds=pbounds, random_state=13) 
+        bayes_optimizer.maximize(init_points=config.init_points, n_iter=config.n_iter,)
+
+    def bayes_opt_config_wrapper(self, **params):
+        for attr, value in params.items():
+            self.config[attr] = value
+
+        return self.train_eval()
+
+    def train_eval(self):
         #turn on tracker
         mlflow.start_run()
 
+        optimizer = config.optimizer(self.model.parameters(), lr=self.config['lr'], momentum=self.config['momentum'])
+        
         for attr, value in self.config.items():
             mlflow.log_param(attr, value)
 
-    def train_eval(self):
-        for epoch in tqdm(range(self.config['epochs'])):
-            for idx, batch in enumerate(tqdm(self.train_loader)):
+        for epoch in (range(self.config['epochs'])):
+            model.train()
+            #print ('train epoch: ', str(epoch))
+            for idx, batch in enumerate(self.train_loader):
                 text, label = batch.document, batch.label
                 text.data.t_()
 
                 prediction = self.model(text)
 
                 loss = self.loss_fn(prediction, label)
-                self.optimizer.zero_grad()
+                optimizer.zero_grad()
                 loss.backward()
-                self.optimizer.step()
+                optimizer.step()
 
                 mlflow.log_metric('train_loss', loss.item())
 
             n_total_data = 0.0
             n_correct = 0.0
 
-            for idx, batch in enumerate(tqdm(self.val_loader)):
+            model.eval()
+            #print ('eval epoch: ', str(epoch))
+            for idx, batch in enumerate(self.val_loader):
                 text, label = batch.document, batch.label
                 text.data.t_()
 
@@ -61,35 +83,47 @@ class Sentiment_train_eval(object):
 
         mlflow.end_run()
 
+        return accuracy
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     #data loader param
     parser.add_argument('--train_file_path', default='../data/naver_movie_sentiment/ratings_train.txt', help='training file path')
     parser.add_argument('--val_file_path', default='../data/naver_movie_sentiment/ratings_test.txt', help='validation file path')
- 
+    parser.add_argument('--batch_size', type=int, default=2048)          #based on 12GB GPU Mem
+
     #model param
     parser.add_argument('--hidden_size', type=int, default=224)         #for filter setting
-    parser.add_argument('--dim', type=int, default=224)                 #for embedding setting
-    parser.add_argument('--n_channel_per_window', type=int, default=3)
+    parser.add_argument('--dim', type=int, default=224)                 #for embedding setting(considering resnet input shape)
+    parser.add_argument('--n_channel_per_window', type=int, default=30)
     parser.add_argument('--label_size', type=int, default=2)
-    parser.add_argument('--dropout', type=float, default='0.5')
+    parser.add_argument('--dropout', type=float, default='0.1')
  
     #experiment param
-    parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, help='SGD momentum (default: 0.5)')
+    parser.add_argument('--epochs', type=int, default=40, help='number of epochs to train (default: 20)')
+    parser.add_argument('--lr', default=(0.005, 0.01), help='learning rate (default: 0.005)')
+    parser.add_argument('--momentum', default=(0.5, 0.9), help='SGD momentum (default: 0.5)')
+    parser.add_argument('--optimizer', default=SGD)
+
+    #bayesian optimization setting
+    parser.add_argument('--init_points', type=int, default=10)
+    parser.add_argument('--n_iter', type=int, default=20)
 
     config = parser.parse_args()
 
-    #data loader ready
-    train_loader, vocab_size = create_data_loader(config.train_file_path, build_vocab=True)
-    val_loader = create_data_loader(config.val_file_path)
+    #set GPU device
+    device = torch.device(0)
+
+    #data loader ready(it takes long time because of building vocab)
+    train_loader, val_loader, vocab_size = create_data_loader(config.train_file_path, config.val_file_path, batchSize=config.batch_size, device=device)
 
     #model ready
-    model = WordCNN(vocab_size, config)
- 
-    optimizer = SGD(model.parameters(), lr=config.lr, momentum=config.momentum)
+    embeddingnet = WordCNN(vocab_size, config)
+    embeddingnet.to(device)
+    model = nn.DataParallel(embeddingnet)
+
     loss_fn = nn.CrossEntropyLoss()
 
-    trainer = Sentiment_train_eval(train_loader, val_loader, optimizer, loss_fn, vars(config))
-    trainer.train_eval()
+    trainer = Sentiment_train_eval(train_loader, val_loader, model, loss_fn, vars(config))
+    #trainer.train_eval()
+    trainer.bayes_opt_train_eval()
